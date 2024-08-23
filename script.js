0a0ace6e3c5e141e0ad19bbcd040314f85b1986b
Didier <115608922+DogGotSwag@users.noreply.github.com>
2024-08-22 20:00:37 -0600

    make button generic again & make form novalidate
    generic so it doesnt refresh the page and
    novalidate so I can validate it myself

--- script.js
+++ script.js
@@ -1,121 +1,114 @@
 const myLibrary = [];
 
-
 class Book {
-    constructor( title , author , pages ,read){
-        this.title = title;
-        this.author = author;
-        this.pages = pages;
-        this.read = read;
-    }
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
 }
 
-function addBookToLibrary( obj ) {
-  myLibrary.push( obj );
+function addBookToLibrary(obj) {
+  myLibrary.push(obj);
 }
 
-let tableBody = document.querySelector('.tableBody');
+let tableBody = document.querySelector(".tableBody");
 
-function displayTable( array ){
-    let childrenArray = Array.from(tableBody.childNodes);
-    console.log();
-    for( let i = 0; i < childrenArray.length; i++){
-        tableBody.removeChild(childrenArray[i]);
+function displayTable(array) {
+  let childrenArray = Array.from(tableBody.childNodes);
+  console.log();
+  for (let i = 0; i < childrenArray.length; i++) {
+    tableBody.removeChild(childrenArray[i]);
+  }
 
-    }
+  for (let i = 0; i < array.length; i++) {
+    let removeButton = document.createElement("button");
+    removeButton.innerText = "✖";
+    removeButton.className = `remove book_${i}`;
 
-    for( let i = 0; i < array.length ; i++ ){
+    let newRow = document.createElement("div");
+    newRow.className = "row";
 
-        let removeButton = document.createElement('button');
-        removeButton.innerText = '✖';
-        removeButton.className = `remove book_${i}`;
+    let title = document.createElement("div");
+    title.className = "title";
+    title.innerText = array[i].title;
 
-        let newRow = document.createElement('div');
-        newRow.className = 'row';
+    let author = document.createElement("div");
+    author.innerText = array[i].author;
+    author.className = "author";
 
-        let title = document.createElement('div');
-        title.className = 'title';
-        title.innerText = array[i].title;
+    let pages = document.createElement("div");
+    pages.innerText = array[i].pages;
 
-        let author = document.createElement('div');
-        author.innerText = array[i].author;
-        author.className = 'author'
+    let read = document.createElement("div");
+    read.innerText = array[i].read == true ? "read" : "unread";
+    read.className = "read";
 
-        let pages = document.createElement('div');
-        pages.innerText = array[i].pages;
+    let haveReadButton = document.createElement("button");
+    haveReadButton.innerText = "Change";
 
-        let read = document.createElement('div');
-        read.innerText = array[i].read == true ? 'read': "unread" ;
-        read.className = 'read';
+    haveReadButton.addEventListener("click", () => {
+      let index = removeButton.classList[1].split("_")[1];
+      myLibrary[index].read = array[i].read == true ? false : true;
+      displayTable(myLibrary);
+    });
 
-        let haveReadButton = document.createElement('button');
-        haveReadButton.innerText = 'Change';
+    read.appendChild(haveReadButton);
 
-        haveReadButton.addEventListener('click', () => {
+    newRow.appendChild(removeButton);
 
-            let index = removeButton.classList[1].split('_')[1];
-            myLibrary[index].read = array[i].read == true ? false: true ;
-            displayTable(myLibrary);
-        });
+    newRow.appendChild(title);
+    newRow.appendChild(author);
+    newRow.appendChild(pages);
+    newRow.appendChild(read);
 
-        read.appendChild(haveReadButton);
+    let frag = new DocumentFragment();
+    frag.appendChild(newRow);
 
-        newRow.appendChild(removeButton);
+    tableBody.appendChild(frag);
 
-        newRow.appendChild(title);
-        newRow.appendChild(author);
-        newRow.appendChild(pages);
-        newRow.appendChild(read)
-        
+    removeButton.addEventListener("click", (event) => {
+      let thisRow = event.target.parentNode;
+      tableBody.removeChild(thisRow);
 
-        let frag = new DocumentFragment();
-        frag.appendChild(newRow);
-
-        tableBody.appendChild(frag);
-
-
-        removeButton.addEventListener('click', ( event )=>{
-            let thisRow = event.target.parentNode;
-            tableBody.removeChild( thisRow );
-
-            let index = removeButton.classList[1].split('_')[1];
-            removeBook( index );
-        });
-
-    }
+      let index = removeButton.classList[1].split("_")[1];
+      removeBook(index);
+    });
+  }
 }
 
-let moby = new Book('Moby Dick', "Herman Melville", 624, true );
+let moby = new Book("Moby Dick", "Herman Melville", 624, true);
 addBookToLibrary(moby);
 
-displayTable( myLibrary);
+displayTable(myLibrary);
 
-let button = document.querySelector('.addButton');
+let button = document.querySelector(".addButton");
 
-let titleInput = document.querySelector('#bookTitle');
-let authorInput = document.querySelector('#bookAuthor');
-let pagesInput = document.querySelector('#bookPages');
-let readInput = document.querySelector('#bookRead');
+let titleInput = document.querySelector("#bookTitle");
+let authorInput = document.querySelector("#bookAuthor");
+let pagesInput = document.querySelector("#bookPages");
+let readInput = document.querySelector("#bookRead");
 
-button.addEventListener('click', () =>{
-    
-    
-    let obj = new Book(titleInput.value, authorInput.value, pagesInput.value, readInput.value);
+button.addEventListener("click", () => {
+  let obj = new Book(
+    titleInput.value,
+    authorInput.value,
+    pagesInput.value,
+    readInput.value
+  );
 
-    titleInput.value = '';
-    authorInput.value = '';
-    pagesInput.value = '';
-    readInput.value = '';
+  titleInput.value = "";
+  authorInput.value = "";
+  pagesInput.value = "";
+  readInput.value = "";
 
-    addBookToLibrary(obj);
+  addBookToLibrary(obj);
 
-    displayTable(myLibrary);
-    console.log( myLibrary);
+  displayTable(myLibrary);
+  console.log(myLibrary);
 });
 
-
-
-function removeBook( index){
-    myLibrary.splice( index, 1);
+function removeBook(index) {
+  myLibrary.splice(index, 1);
 }
-
